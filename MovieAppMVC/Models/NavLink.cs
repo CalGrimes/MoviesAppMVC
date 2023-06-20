855e62755d8314890afedfc2e0ca73fc358f6ef2
Cal Grimes <cgrimes@vldbsolutions.com>
2023-06-20 09:25:15 +0100

    Active NavItem | Theme Icon
    - Added an indicator for active page under nav item.
    
    - Added an animated theme icon

---
+++ MovieAppMVC/Models/NavLink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MovieAppMVC.Models
+{
+    public class NavLink
+    {
+        public string Text { get; set; }
+        public string Area { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public bool IsActive { get; set; }
+
+        public NavLink(string text, string action, string controller, string area = null)
+        {
+            Text = text;
+            Area = area;
+            Controller = controller;
+            Action = action;
+        }
+    }
+}
+
